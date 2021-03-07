f5b87aa00a5c66a0c191d0771dd5b71ae2523141
Roee Shachar <shacharroy14@gmail.com>
2021-03-07 14:06:26 +0200

    feat: add logger (#5)
    * feat: add logger
    
    * chore: add eslint and basic tests
    
    * fix: enter in EOF, remove eslintignore file
    
    * fix: enter in EOF
    
    * fix: lint script

--- src/cleanup_script.js
+++ src/cleanup_script.js
@@ -1,1 +1,12 @@
-// TODO: add cleanup logic
+const logger = require('./logger');
+
+async function main() {
+  // TODO: add logic
+}
+
+main()
+  .then(() => logger.info('Successfully completed cleanup job'))
+  .catch((error) => {
+    logger.error('An error occured: ' + error.message);
+    process.exit(1);
+  });
