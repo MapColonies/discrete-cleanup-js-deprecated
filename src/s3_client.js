95e4f73dfc211f057fd634e3fe6a48eb18769b2e
Anguiar <shacharroy14@gmail.com>
2021-03-09 16:03:48 +0200

    fix: lint

--- src/s3_client.js
+++ src/s3_client.js
@@ -1,10 +1,10 @@
-const S3 = require("aws-sdk/clients/s3");
-const config = require("config");
+const S3 = require('aws-sdk/clients/s3');
+const config = require('config');
 let s3Client;
 
 function getS3Client() {
   if (!s3Client) {
-    const s3Config = config.get("s3");
+    const s3Config = config.get('s3');
     const clientConfig = {
       apiVersion: s3Config.apiVersion,
       endpoint: s3Config.endpoint,
@@ -12,7 +12,7 @@ function getS3Client() {
       secretAccessKey: s3Config.secretAccessKey,
       maxRetries: s3Config.maxRetries,
       sslEnabled: s3Config.sslEnabled,
-      s3ForcePathStyle: true,
+      s3ForcePathStyle: true
     };
     s3Client = new S3(clientConfig);
   }
