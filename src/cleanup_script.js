1b83e70186e2388c092e55c68a4833d6ab174234
Roee Shachar <shacharroy14@gmail.com>
2021-03-09 16:26:14 +0200

    feat: add support of s3 deletion (#7)
    * feat: add support of s3 deletion
    
    * fix: no prettier
    
    * fix: lint
    
    * fix: lint
    
    * chore: remove pretty-quick

--- src/cleanup_script.js
+++ src/cleanup_script.js
@@ -1,13 +1,18 @@
 const config = require('config');
 const axios = require('axios');
+const { promises } = require('fs');
+const s3Client = require('./s3_client');
 const logger = require('./logger');
-const fsPromise = require('fs').promises;
+
+function getDataFromDB(url) {
+  const dbConfig = config.get('db');
+  const urlToExecute = `${dbConfig.url}/${url}`;
+  logger.info(`Retrieving not cleaned tasks from DB in url ${urlToExecute}`);
+  return axios.get(urlToExecute);
+}
 
 async function getNotCleanedTasks() {
-  const dbConfig = config.get('db');
-  const notCleanedTasksURL = `${dbConfig.url}/discrete?cleaned=false`;
-  logger.info(`Retrieving not cleaned tasks from DB in url ${notCleanedTasksURL}`);
-  const result = await axios.get(notCleanedTasksURL);
+  const result = await getDataFromDB('discrete?cleaned=false');
   return result.data;
 }
 
@@ -23,7 +28,7 @@ function getURIsArray(discreteArray) {
 }
 
 function deleteFiles(pathArray) {
-  const promiseDeleteArray = pathArray.map((path) => fsPromise.unlink(path));
+  const promiseDeleteArray = pathArray.map((path) => promises.unlink(path));
   return Promise.all(promiseDeleteArray);
 }
 
@@ -33,7 +38,7 @@ async function deleteChunks(urisArray) {
 
   for (let i = 0; i < urisArray.length; i += chunkSize) {
     chunkedArray = urisArray.slice(i, i + chunkSize);
-    logger.info(`Deleting files from FS in paths: ${chunkedArray.join(' | ')}`);
+    logger.info(`Deleting files from FS in paths: ["${chunkedArray.join('", "')}"]`);
     await deleteFiles(chunkedArray);
   }
 }
@@ -44,8 +49,55 @@ async function deleteOriginalFiles() {
   await deleteChunks(allURIs);
 }
 
+async function s3MainDeleteLoop() {
+  const discretes = await getItemsToDelete();
+  for (const discrete of discretes) {
+    const id = discrete.id;
+    const version = discrete.version;
+    await deleteS3WithBatch(`${id}/${version}`);
+  }
+}
+
+async function getItemsToDelete() {
+  const result = await getDataFromDB('discrete?cleaned=false&status=failed');
+  return result.data;
+}
+
+async function deleteS3WithBatch(Prefix) {
+  const BATCH_SIZE = config.get('s3').deleteBatchSize;
+  let { prepareItemsToDeletion, ContinuationToken } = await parseItemsFromS3(Prefix);
+
+  while (prepareItemsToDeletion.length !== 0) {
+    const s3DeletePromiseArray = [];
+
+    for (let i = 0; i < BATCH_SIZE; i++) {
+      s3DeletePromiseArray.push(deleteFromS3(prepareItemsToDeletion));
+    }
+
+    await Promise.all(s3DeletePromiseArray);
+    prepareItemsToDeletion = (await parseItemsFromS3(Prefix, ContinuationToken)).prepareItemsToDeletion;
+  }
+}
+
+async function parseItemsFromS3(Prefix, ContinuationToken) {
+  const s3 = config.get('s3');
+  logger.info(`Listing ${s3.listObjectMaxKeys} objects from bucket ${s3.bucket}`);
+  const res = await s3Client.listObjectsV2({ Bucket: s3.bucket, MaxKeys: s3.listObjectMaxKeys, Prefix, ContinuationToken }).promise();
+  const prepareItemsToDeletion = res.Contents.map((content) => {
+    return { Key: content.Key };
+  });
+  return { prepareItemsToDeletion, ContinuationToken: res.NextContinuationToken };
+}
+
+async function deleteFromS3(Objects) {
+  const bucket = config.get('s3').bucket;
+  logger.info(`Deleting ${JSON.stringify(Objects)} objects from bucket ${bucket}`);
+  return s3Client.deleteObjects({ Bucket: bucket, Delete: { Objects } }).promise();
+}
+
 async function main() {
   await deleteOriginalFiles();
+  await s3MainDeleteLoop();
 }
 
 main()
