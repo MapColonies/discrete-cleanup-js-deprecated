bc673fb3f5b26d2091c59c83bbda53e0608fbe3c
Anguiar <shacharroy14@gmail.com>
2021-03-10 14:42:44 +0200

    chore: add prettier config

--- src/cleanup_script.js
+++ src/cleanup_script.js
@@ -17,9 +17,7 @@ async function getNotCleanedTasks() {
 }
 
 function getURIsArray(discreteArray) {
-  const fileURIsARray = discreteArray.map(
-    (discrete) => discrete.metadata.fileUris
-  );
+  const fileURIsARray = discreteArray.map((discrete) => discrete.metadata.fileUris);
   const allURIs = [];
   for (const uriArray of fileURIsARray) {
     for (const uri of uriArray) {
@@ -40,9 +38,7 @@ async function deleteChunks(urisArray) {
 
   for (let i = 0; i < urisArray.length; i += chunkSize) {
     chunkedArray = urisArray.slice(i, i + chunkSize);
-    logger.info(
-      `Deleting files from FS in paths: ['${chunkedArray.join('', '')}']`
-    );
+    logger.info(`Deleting files from FS in paths: ['${chunkedArray.join('', '')}']`);
     await deleteFiles(chunkedArray);
   }
 }
@@ -70,9 +66,7 @@ async function getItemsToDelete() {
 
 async function deleteS3WithBatch(Prefix) {
   const BATCH_SIZE = config.get('s3').deleteBatchSize;
-  let { prepareItemsToDeletion, ContinuationToken } = await parseItemsFromS3(
-    Prefix
-  );
+  let { prepareItemsToDeletion, ContinuationToken } = await parseItemsFromS3(Prefix);
 
   while (prepareItemsToDeletion.length !== 0) {
     const s3DeletePromiseArray = [];
@@ -82,22 +76,19 @@ async function deleteS3WithBatch(Prefix) {
     }
 
     await Promise.all(s3DeletePromiseArray);
-    prepareItemsToDeletion = (await parseItemsFromS3(Prefix, ContinuationToken))
-      .prepareItemsToDeletion;
+    prepareItemsToDeletion = (await parseItemsFromS3(Prefix, ContinuationToken)).prepareItemsToDeletion;
   }
 }
 
 async function parseItemsFromS3(Prefix, ContinuationToken) {
   const s3 = config.get('s3');
-  logger.info(
-    `Listing ${s3.listObjectMaxKeys} objects from bucket ${s3.bucket}`
-  );
+  logger.info(`Listing ${s3.listObjectMaxKeys} objects from bucket ${s3.bucket}`);
   const res = await s3Client
     .listObjectsV2({
       Bucket: s3.bucket,
       MaxKeys: s3.listObjectMaxKeys,
       Prefix,
-      ContinuationToken
+      ContinuationToken,
     })
     .promise();
   const prepareItemsToDeletion = res.Contents.map((content) => {
@@ -105,25 +96,19 @@ async function parseItemsFromS3(Prefix, ContinuationToken) {
   });
   return {
     prepareItemsToDeletion,
-    ContinuationToken: res.NextContinuationToken
+    ContinuationToken: res.NextContinuationToken,
   };
 }
 
 async function deleteFromS3(Objects) {
   const bucket = config.get('s3').bucket;
-  logger.info(
-    `Deleting ${JSON.stringify(Objects)} objects from bucket ${bucket}`
-  );
-  return s3Client
-    .deleteObjects({ Bucket: bucket, Delete: { Objects } })
-    .promise();
+  logger.info(`Deleting ${JSON.stringify(Objects)} objects from bucket ${bucket}`);
+  return s3Client.deleteObjects({ Bucket: bucket, Delete: { Objects } }).promise();
 }
 
 async function deleteMapProxyLayer(layerName) {
   const mapproxyUrl = config.get('mapproxy_api').url;
-  logger.info(
-    `Deleting layer [${layerName}] from mapproxy in path [${mapproxyUrl}]`
-  );
+  logger.info(`Deleting layer [${layerName}] from mapproxy in path [${mapproxyUrl}]`);
   await axios.delete(`${mapproxyUrl}/layer/${layerName}`);
 }
 
